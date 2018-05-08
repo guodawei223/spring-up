21b6ea65d30f2321bb7afb00c476ec04cf93ea1a
Lang <silentbalanceyh@126.com>
2018-05-08 23:46:30 +0800

    Feign

--- spring-co/src/main/java/io/spring/up/jhipster/config/FeignConfiguration.java
+++ spring-co/src/main/java/io/spring/up/jhipster/config/FeignConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients(basePackages = "com.mbcloud.auth")
+@EnableFeignClients(basePackages = "com")
 public class FeignConfiguration {
 
     /**
